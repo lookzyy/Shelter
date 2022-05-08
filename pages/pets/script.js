6f0cb5ce5106bd40d2afce08f2b9e583fb6a0b64
lookzyy <luka.joxa@gmail.com>
2022-05-09 00:47:01 +0400

    fix:button hover

---
+++ pages/pets/script.js
@@ -0,0 +1,43 @@
+document.querySelectorAll('.learn-more').forEach(btnElement => {
+    btnElement.addEventListener("click", (e) => {
+        learnMoreFunction(e);
+    });
+});
+
+document.querySelector(".modal-close-button").addEventListener("click", hideModal);
+document.querySelector(".modal-background").addEventListener("click", hideModal);
+
+function learnMoreFunction(e) {
+    var id = e.target.id;
+
+    fetch("../../assets/json/pets.json")
+        .then(response => response.json())
+        .then(data => {
+            var petInfo = data[id.replace("button-", "")];
+            var modalContent = buildModalContent(petInfo);
+
+            document.querySelector(".modal-inner-content-div").innerHTML = modalContent;
+
+            showModal()
+        })
+};
+
+function hideModal() {
+    document.querySelector(".modal-container").style.display = "none";
+}
+
+function showModal() {
+    document.querySelector(".modal-container").style.display = "flex";
+}
+
+function buildModalContent(petInfo) {
+    return document.getElementById("modal-template").innerHTML
+        .replace("{{imgUrl}}", petInfo.imgUrl)
+        .replace("{{breed}}", petInfo.breed)
+        .replace("{{description}}", petInfo.description)
+        .replace("{{age}}", petInfo.age)
+        .replace("{{inoculations}}", petInfo.inoculations)
+        .replace("{{diseases}}", petInfo.diseases)
+        .replace("{{parasites}}", petInfo.parasites)
+        .replace("{{title}}", petInfo.title);
+}
